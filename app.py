fe0a1984f1338414d823dc6c815b0e73f97b6020
t19ty <127368044+t19ty@users.noreply.github.com>
2025-07-17 16:10:02 +0800

    Update app.py
    from dotenv import load_dotenv
    load_dotenv()
    
    removed

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import re
 import os
 import random
 import uuid
-from dotenv import load_dotenv
-load_dotenv()
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
 SESSION_SECRET = os.environ.get("SESSION_SECRET")
